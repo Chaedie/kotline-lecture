69795bb6dfab52f6cfbe345e58f181f72652feef
chaedongim <chaedong.im@cj.net>
2025-07-21 09:07:43 +0900

    chore: spring 프로젝트 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+    id("org.springframework.boot") version "3.2.3"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "1.8.0"
+
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "com.chaedie"
@@ -10,6 +14,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.2.3")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
     testImplementation(kotlin("test"))
 }
 
