b5bafe9ca65b270b470e6d02584503fa21a81afc
chaedongim <chaedong.im@cj.net>
2025-07-21 09:31:02 +0900

    feat: User, Account Entity 설정

---
+++ src/main/kotlin/types/entity/Account.kt
@@ -0,0 +1,30 @@
+package com.chaedie.types.entity
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "account")
+class Account (
+    @Id
+    @Column(name = "ulid", length = 12, nullable = false)
+    val ulid: String,
+
+    // TODO:
+    @Column(name = "balance", nullable = false, precision = 15, scale = 2)
+    var balance: BigDecimal = BigDecimal.ZERO,
+
+    @Column(name = "account_number", length = 100, nullable = false, unique = true)
+    val accountNumber: String,
+
+    @Column(name = "is_deleted", nullable = false)
+    val isDeleted: Boolean = false,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+){
+}
---
+++ src/main/kotlin/types/entity/User.kt
@@ -0,0 +1,29 @@
+package com.chaedie.types.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "user")
+class User (
+    @Id
+    @Column(name = "ulid", length = 12, nullable = false)
+    val ulid: String,
+
+    @Column(name = "platform", nullable = false, length = 25)
+    val platform: String,
+
+    @Column(name = "username", nullable = false, length = 50, unique = true)
+    val username: String,
+
+    @Column(name = "access_token", length = 255)
+    val accessToken: String? = null,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
+){
+
+}
